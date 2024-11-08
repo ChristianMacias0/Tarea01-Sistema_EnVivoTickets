9762d64ec10ef573c85db74359b9708ad24d6852
Christian Macias <cjavmaci@espol.edu.ec>
2024-11-07 22:34:29 -0500

    Update clases de usuarios

--- src/sistema_envivotickets/Usuarios/Administrador.java
+++ src/sistema_envivotickets/Usuarios/Administrador.java
@@ -1,5 +1,25 @@
 package sistema_envivotickets.Usuarios;
 
-public class Administrador {
+import sistema_envivotickets.Sistemas.*;
+
+public class Administrador extends User{
+
+    //METODOS
+
+    public void agregarEvento(Evento evento){
+        //Agregar evento a la base de datos
+    }
+    public void eliminarEvento(Evento evento){
+        //Eliminar evento de la base de datos
+    }
+    public void gestionarPreciosPromociones(){
+        //Gestionar precios de las promociones de eventos
+    }
+    public void gestionarPoliticasCancelacion(String politicas){
+        //Gestionar politicas de cancelacion de eventos
+    }
+    public void gestionarConfiguracionEvento(){
+        //Gestionar configuracion de eventos
+    }
 
 }
