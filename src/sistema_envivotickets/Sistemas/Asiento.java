b1644ae59b1715effde600ea65ccb24fb0c784b3
Paulo Tapia <pmtapia@espol.edu.ec>
2024-11-07 22:41:24 -0500

    updateAsiento
    creacion de atributos y metodo mostrarInformacion

--- src/sistema_envivotickets/Sistemas/Asiento.java
+++ src/sistema_envivotickets/Sistemas/Asiento.java
@@ -1,5 +1,34 @@
 package sistema_envivotickets.Sistemas;
+import sistema_envivotickets.Enums.Estado;
 
 public class Asiento {
-    
+    private int id;
+    private String numero;
+    private Seccion seccion;
+    private Estado estado;
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNumero(){
+        return numero;
+    }
+
+    public Seccion getSeccion(){
+        return seccion;
+    }
+
+    public Estado getEstado(){
+        return estado;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("ID: " + id);
+        System.out.println("Número: " + numero);
+        System.out.println("Sección: " + seccion.getNombre());
+        System.out.println("Estado: " + estado);
+    }
+
+
 }
