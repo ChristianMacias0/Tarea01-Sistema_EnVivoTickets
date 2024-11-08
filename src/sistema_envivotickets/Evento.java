4ff612aa6ae9f9384bf3f991296e3c260397acd8
Paulo Tapia <pmtapia@espol.edu.ec>
2024-11-07 22:13:17 -0500

    Actualizacion clase Evento
    Colocacion de atributos y sus getters

--- src/sistema_envivotickets/Evento.java
+++ src/sistema_envivotickets/Evento.java
@@ -1,5 +1,28 @@
 package sistema_envivotickets;
 
+
 public class Evento {
-    
+
+    private int id;
+    private String nombre;
+    private String tipo;
+    private String descripcion;
+
+
+
+    //Getters
+
+    public int getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getTipo() {
+        return tipo;
+    }
+    public int getDescripcion() {
+        return descripcion;
+    }
+
 }
