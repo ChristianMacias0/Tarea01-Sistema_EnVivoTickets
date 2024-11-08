97830bde7db39de0b734d729c9019c34133e045c
Christian Macias <cjavmaci@espol.edu.ec>
2024-11-07 22:49:42 -0500

    Update en clase evento

---
+++ src/sistema_envivotickets/Estado.java
@@ -0,0 +1,5 @@
+package sistema_envivotickets.Enums;
+
+public enum Estado {
+    DISPONIBLE , RESERVADO , AGOTADO
+}
--- src/sistema_envivotickets/Sistemas/Evento.java
+++ src/sistema_envivotickets/Sistemas/Evento.java
@@ -1,14 +1,27 @@
-package sistema_envivotickets;
-
+package sistema_envivotickets.Sistemas;
 
 public class Evento {
-
     private int id;
     private String nombre;
     private String tipo;
     private String descripcion;
 
-
+    //METODOS
+    public void agregarEvento(Evento evento){
+        //Agregar evento a la base de datos
+    }
+    public void eliminarEvento(Evento evento){
+        //Eliminar evento de la base de datos
+    }
+    public void gestionarPreciosPromociones(){
+        //Gestionar precios de las promociones de eventos
+    }
+    public void gestionarPoliticasCancelacion(String politicas){
+        //Gestionar politicas de cancelacion de eventos
+    }
+    public void gestionarConfiguracionEvento(){
+        //Gestionar configuracion de eventos
+    }
 
     //Getters
 
